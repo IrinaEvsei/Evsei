4d0645efa1e4a97683d0cf2d5e1d691b3390ccc5
Irina Evsei <irinaesei@mail.ru>
2015-10-12 22:36:11 +0200

    First commit

---
+++ Evsei/ZAdanie1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+int main(){
+	int a,b=0;
+	cin >> a;
+	int a2 = 0;
+	while (a)
+	{
+		a2 = a2 * 10 + a % 10;
+		a /= 10;
+	}
+	//cout << a2;
+	while (a2 % 10)
+	{
+		b += a2 % 10;
+		a2 /= 10;
+	}
+	cout << b << std::endl;
+	return 0;
+}
---
+++ Evsei/Zadanie2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+int main(){
+	int a, a1, a2=0;
+	cin >> a; 
+	a1 = a;
+	while (a)
+	{
+		a2 =a2*10 + a % 10;
+		a /= 10;
+	}
+	cout << (a1==a2 ? "yes" : "no");
+	return 0;
+}
